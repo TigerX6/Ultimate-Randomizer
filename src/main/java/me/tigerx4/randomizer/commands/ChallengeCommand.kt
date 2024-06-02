9ae139f0c216c586a436c9b302ed046bcad16cf7
TigerX6 <tom@jsmt.de>
2024-06-02 12:49:57 +0200

    - added plugin message configurability - minor tweaks

--- src/main/java/me/tigerx4/randomizer/commands/ChallengeCommand.kt
+++ src/main/java/me/tigerx4/randomizer/commands/ChallengeCommand.kt
@@ -2,7 +2,6 @@ package me.tigerx4.randomizer.commands
 
 import me.tigerx4.randomizer.Randomizer
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
@@ -22,113 +21,103 @@ class ChallengeCommand(plugin: Randomizer) : TabExecutor {
     var challengeStatus = "end"
     private val config: FileConfiguration = plugin.config
     private var mm = MiniMessage.miniMessage()
-    private val prefix: Component = Component.text("[")
-        .append(mm.deserialize("<rainbow>Randomizer</rainbow>"))
-        .append(Component.text("] "))
+    private val prefix: Component = mm.deserialize("${config.getString("plugin-messages.prefix")}")
+        .append(Component.text(" "))
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 
         if (sender !is Player) return false
-        if (!sender.hasPermission("Randomizer.randomizer")) {
-            sender.sendMessage(prefix.append(
-                Component.text("ERROR: ", NamedTextColor.RED)
-                    .decorate(TextDecoration.BOLD)
+        fun sendPermissionError() {
+            sender.sendMessage(
+                prefix.append(
+                    mm.deserialize("${config.getString("plugin-messages.permission-error")}")
+                )
             )
-                .append(Component.text("You don't have the required permission to use that", NamedTextColor.RED)))
-            return true
+        }
+
+        fun sendArgsError() {
+            sender.sendMessage(
+                prefix.append(
+                    mm.deserialize("${config.getString("plugin-messages.argument-error")}")
+                )
+            )
         }
 
         if (args.size != 1) {
-            sender.sendMessage(
-                prefix.append(
-                    Component.text("ERROR: ", NamedTextColor.RED)
-                        .decorate(TextDecoration.BOLD)
-                )
-                    .append(Component.text("Could not run with the arguments supplied", NamedTextColor.RED))
-            )
+            sendArgsError()
             return false
         }
 
         if (args[0] == "start") {
-            if (challengeStatus == "start") {
-                sender.sendMessage(
-                    prefix.append(
-                        Component.text("ERROR: ", NamedTextColor.RED)
-                            .decorate(TextDecoration.BOLD)
+            if (sender.hasPermission("randomizer.start")) {
+                if (challengeStatus == "start") {
+                    sender.sendMessage(
+                        prefix.append(
+                            mm.deserialize("${config.getString("plugin-messages.already-enabled")}")
+                        )
                     )
-                        .append(Component.text("Randomizer is already ON", NamedTextColor.RED))
+                    return true
+                }
+
+                challengeStatus = "start"
+                Bukkit.broadcast(
+                    prefix
+                        .append(mm.deserialize("${config.getString("plugin-messages.randomizer-on")}"))
                 )
+                if (config.getBoolean("show_timer")) {
+                    stopTimer()
+                    startTimer()
+                }
                 return true
             }
-
-            challengeStatus = "start"
-            Bukkit.broadcast(
-                prefix
-                    .append(Component.text("Randomizer is now "))
-                    .append(
-                        Component.text("ON", NamedTextColor.GREEN)
-                            .decorate(TextDecoration.BOLD)
-                    )
-            )
-            if (config.getBoolean("show_timer")) {
-                stopTimer()
-                startTimer()
-            }
-            return true
+            sendPermissionError()
         }
 
         if (args[0] == "stop") {
-            if (challengeStatus == "end") {
-                sender.sendMessage(
-                    prefix.append(
-                        Component.text("ERROR: ", NamedTextColor.RED)
-                            .decorate(TextDecoration.BOLD)
+            if (sender.hasPermission("randomizer.stop")) {
+                if (challengeStatus == "end") {
+                    sender.sendMessage(
+                        prefix.append(
+                            mm.deserialize("${config.getString("plugin-messages.already-disabled")}")
+                        )
                     )
-                        .append(Component.text("Randomizer is already OFF", NamedTextColor.RED))
+                    return true
+                }
+
+                challengeStatus = "end"
+                Bukkit.broadcast(
+                    prefix
+                        .append(mm.deserialize("${config.getString("plugin-messages.randomizer-off")}"))
                 )
+
+                if (config.getBoolean("show_timer")) {
+                    stopTimer()
+                }
                 return true
             }
-
-            challengeStatus = "end"
-            Bukkit.broadcast(
-                prefix
-                    .append(Component.text("Randomizer is now "))
-                    .append(
-                        Component.text("OFF", NamedTextColor.RED)
-                            .decorate(TextDecoration.BOLD)
-                    )
-            )
-            if (config.getBoolean("show_timer")) {
-                stopTimer()
-            }
-            return true
+            sendPermissionError()
         }
 
-        sender.sendMessage(
-            prefix.append(
-                Component.text("ERROR: ", NamedTextColor.RED)
-                    .decorate(TextDecoration.BOLD)
-            )
-                .append(Component.text("Could not run with the arguments supplied. ", NamedTextColor.RED))
-        )
+        sendArgsError()
         return false
     }
 
     private val executorService: ScheduledExecutorService = Executors.newScheduledThreadPool(0)
     private var scheduledFuture: ScheduledFuture<*>? = null
     private var elapsedS = 0.seconds
+    var timerText = ""
 
     private fun startTimer() {
         scheduledFuture = executorService.scheduleAtFixedRate(
             object : TimerTask() {
                 override fun run() {
                     elapsedS += 1.seconds
-                    val timeText = elapsedS.toComponents { hours, minutes, seconds, _ ->
+                    timerText = elapsedS.toComponents { hours, minutes, seconds, _ ->
                         String.format("%02d : %02d : %02d", hours, minutes, seconds)
                     }
                     for (player in Bukkit.getOnlinePlayers()) {
                         player.sendActionBar(
-                            mm.deserialize("<gradient:yellow:gold:1>$timeText</gradient>")
+                            mm.deserialize("<gradient:yellow:gold:1>$timerText")
                                 .decorate(TextDecoration.BOLD)
                         )
                     }
