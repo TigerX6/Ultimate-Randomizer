29d369290cb46b4b1e4bae523a3b154017a063b9
TigerX6 <tom@jsmt.de>
2025-09-07 12:34:38 +0200

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,44 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+    kotlin("jvm") version "2.2.20-RC"
+    id("com.gradleup.shadow") version "8.3.0"
     id("xyz.jpenilla.run-paper") version "2.3.1"
-    id("com.gradleup.shadow") version "9.0.0-beta6"
 }
 
 group = "io.github.tigerx6"
-version = "1.0"
+version = "1.5.2"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
-}
-
-kotlin {
-    jvmToolchain(17)
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
     runServer {
         minecraftVersion("1.21.4")
+    }
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
 rootProject.name = "Randomizer"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://repo.papermc.io/repository/maven-public/")
-    }
-}
